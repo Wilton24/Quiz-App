17e31ebed64937918faca1b604dbe51dddc0f4e4
Wilton Batiquin <cbasher24@gmail.com>
2025-07-02 21:58:32 +0800

    clean spaces

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,22 +4,20 @@ import Option from "./Option";
 import Progress from "./Progress";
 
 
-export default function Quiz({activeQuestion, questionIndex, handleAnswerQuestion, isStartGame, nextQuestion}) {
+export default function Quiz({ activeQuestion, questionIndex, handleAnswerQuestion, isStartGame, nextQuestion }) {
   return (
-    <>    
+    <>
       <div className="quiz-card-container">
-          <div className="quiz-card">
-            <Progress key={questionIndex.currentIndex} isStartGame={isStartGame} nextQuestion={nextQuestion}/>            
-            <h2 className="question">{activeQuestion[questionIndex.currentIndex].text}</h2>
-            <ul className="options-list">
-                <Option 
-                  activeQuestion={activeQuestion}
-                  questionIndex={questionIndex}
-                  handleAnswerQuestion={handleAnswerQuestion} />
-            </ul>
-          </div>          
-        
-
+        <div className="quiz-card">
+          <Progress key={questionIndex.currentIndex} isStartGame={isStartGame} nextQuestion={nextQuestion} />
+          <h2 className="question">{activeQuestion[questionIndex.currentIndex].text}</h2>
+          <ul className="options-list">
+            <Option
+              activeQuestion={activeQuestion}
+              questionIndex={questionIndex}
+              handleAnswerQuestion={handleAnswerQuestion} />
+          </ul>
+        </div>
       </div>
     </>
 
