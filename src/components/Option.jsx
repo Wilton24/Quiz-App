08f5e0b614f7637b0b619f3fee6d10acbf470321
Wilton Batiquin <cbasher24@gmail.com>
2025-07-02 22:09:27 +0800

    adding comments for future reference

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,8 +1,11 @@
 
 export default function Option({ activeQuestion, questionIndex, handleAnswerQuestion }) {
 
+  // Shuffle the answers for the current question
   const shuffledQuestion = activeQuestion[questionIndex.currentIndex].answers.sort(() => Math.random() - 0.5);
-  let buttonClassName = "bg-[rgb(140,83,69)] cursor-pointer block w-full text-left text-lg p-[7px] rounded-[10px] m-[10px]"
+
+  // Initial Class name for the button
+  let buttonClassName = "bg-[rgb(140,83,69)] cursor-pointer block w-full text-left text-lg p-[7px] rounded-[10px] m-[10px]";
 
 
   return (
