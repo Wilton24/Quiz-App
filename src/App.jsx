f974b9e1e2b223ec1c70a8e5b3d9691bbc0f8019
Wilton Batiquin <cbasher24@gmail.com>
2025-07-03 21:46:33 +0800

    Timer Initialized in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,68 +11,71 @@ function App() {
     currentScore: 0,
     currentIndex: 0,
     isLastIndex: false
-  });  
+  });
 
   const [isStartGame, setIsStartGame] = useState(false);
   const [playername, setPlayername] = useState('');
 
+  const [isTimerExpired, setIsTimerExpired] = useState(false);
+  const [timer, setTimer] = useState(3000); // 3 seconds timer
+
   const quizFinished = questionIndex.currentIndex === activeQuestion.length - 1;
 
   function handleAnswerQuestion(answer) {
-    if(answer === activeQuestion[questionIndex.currentIndex].answers[0]) {
-      setActiveQuestionIndex(prevData => ({...prevData, currentIndex: prevData.currentIndex + 1, currentScore: prevData.currentScore + 1}));
+    if (answer === activeQuestion[questionIndex.currentIndex].answers[0]) {
+      setActiveQuestionIndex(prevData => ({ ...prevData, currentIndex: prevData.currentIndex + 1, currentScore: prevData.currentScore + 1 }));
     } else {
       nextQuestion();
-    }            
+    }
   };
 
-  function nextQuestion(){
-    setActiveQuestionIndex(prevData => ({...prevData, currentIndex: prevData.currentIndex + 1}));
+  function nextQuestion() {
+    setActiveQuestionIndex(prevData => ({ ...prevData, currentIndex: prevData.currentIndex + 1 }));
   }
 
-  function handleSubmitPlayerName(playername){
-    if(playername.trim() === '') {
+  function handleSubmitPlayerName(playername) {
+    if (playername.trim() === '') {
       alert('Name cannot be blank');
       return;
     }
-    setPlayername(playername)    
+    setPlayername(playername)
   };
 
-  function startGame(){
-    if(playername.trim() === '') {
+  function startGame() {
+    if (playername.trim() === '') {
       alert('Please enter your name');
       return;
     }
-    setIsStartGame(true);    
+    setIsStartGame(true);
   }
 
-  useCallback(()=>{
-    
-  }, [])  
+  // useCallback(() => {
+
+  // }, [])
 
   let content = null;
-  if(isStartGame === false){
-    content = <WelcomeCard 
-        playername={playername}
-        handleSubmitPlayerName={handleSubmitPlayerName}
-        startGame={startGame}
-        />
-  } else{
+  if (isStartGame === false) {
+    content = <WelcomeCard
+      playername={playername}
+      handleSubmitPlayerName={handleSubmitPlayerName}
+      startGame={startGame}
+    />
+  } else {
     content = quizFinished == true ?
-       <Result 
+      <Result
         score={questionIndex.currentScore}
-        totalitems={activeQuestion.length} /> : 
-       <Quiz 
+        totalitems={activeQuestion.length} /> :
+      <Quiz
         activeQuestion={activeQuestion}
-        questionIndex={questionIndex} 
+        questionIndex={questionIndex}
         handleAnswerQuestion={handleAnswerQuestion}
         isStartGame={isStartGame}
-        nextQuestion={nextQuestion}/>
+        nextQuestion={nextQuestion} />
   }
 
-  return(
+  return (
     <>
-      < Header /> 
+      < Header />
       {content}
     </>
   )
